4fa1411cc261484066ec6bc4adf8f369e37cc230
rashaabdulrazzak <rasha.abdulrazzak.com>
2023-09-13 11:47:05 +0300

    add geolocator package and configure the android files to work with

--- lib/main.dart
+++ lib/main.dart
@@ -42,20 +42,27 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Current Location: '),
+              SizedBox(
+                height: 25,
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              ),
+            ],
+          ),
         ));
   }
 }
